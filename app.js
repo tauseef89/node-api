b63cfcbe7771587516f5b14960f0c5a84f72bdd4
tauseef89 <tauseef.n89@gmail.com>
2020-09-05 11:51:01 +0530

    node rest api part 1

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// routes
+app.get('/', (req, res) => {
+    res.send('home page');
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
