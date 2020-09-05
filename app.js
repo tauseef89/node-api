eff5f288b7d9fab7342185d11430790c5dc9d8c3
tauseef89 <tauseef.n89@gmail.com>
2020-09-05 11:52:31 +0530

    node rest api part 1 update

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send('home page');
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
